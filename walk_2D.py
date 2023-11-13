6ac026e7e81e3e0d694b97a2199c6f300267366c
kjes4pres <stangelandkjersti@gmail.com>
2023-11-13 12:49:03 +0100

    Completed task 2b)

--- walk_2D.py
+++ walk_2D.py
@@ -39,4 +39,46 @@ ax.set_title("Random Walk in 2D")
 ax.grid()
 ax.axis("equal")
 
+# plt.savefig("random_walk_2D.png")
+# plt.show()
+
+
+def many_random_walks_2D(M=5, N=500) -> np.ndarray:
+    """
+    Simulation of many random walkers in 2D-
+
+    Input:
+    M : number of walkers (int)
+
+    N : number of steps each walker takes (int)
+
+    Output:
+    r: numpy array, storing the positions of each walker.
+    """
+
+    r = np.zeros((N + 1, 2, M))
+    dr = [(1, 0), (0, 0), (-1, 0), (0, 1), (0, 0), (0, -1)]
+
+    for i in range(M):
+        for j in range(1, N + 1):
+            step = dr[np.random.randint(6)]
+            r[j, 0, i] = r[j - 1, 0, i] + step[0]
+            r[j, 1, i] = r[j - 1, 1, i] + step[1]
+
+    return r
+
+
+R = many_random_walks_2D()
+X = R[:, 0, :]
+Y = R[:, 1, :]
+
+fig, ax = plt.subplots()
+ax.plot(X, Y)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Random Walk in 2D")
+ax.grid()
+ax.axis("equal")
+
+plt.savefig("many_random_2D.png")
 plt.show()
